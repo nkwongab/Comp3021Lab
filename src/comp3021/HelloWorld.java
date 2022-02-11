c939c9a445b43e5e2c9ab0304c84c3631cd56efe
nkwongab <nkwongab@connect.ust.hk>
2022-02-11 15:38:44 +0800

    HelloWorld.java updated.

--- src/comp3021/HelloWorld.java
+++ src/comp3021/HelloWorld.java
@@ -1,8 +1,17 @@
 package comp3021;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("Hello World.\nThe time now is:");
+		test();
+	}
+
+	private static void test(){
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(dtf.format(now));
 	}
 }
